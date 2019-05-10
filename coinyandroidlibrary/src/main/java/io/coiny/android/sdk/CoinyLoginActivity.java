c0bf62b68ddc146dea0312abb783f77ef238b591
Ömral Cörüt <omral@ovidos.com>
2019-05-10 11:06:15 +0300

    Lower Than Nougat Android Version Bug

--- coinyandroidlibrary/src/main/java/io/coiny/android/sdk/CoinyLoginActivity.java
+++ coinyandroidlibrary/src/main/java/io/coiny/android/sdk/CoinyLoginActivity.java
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -54,6 +56,20 @@ public class CoinyLoginActivity extends Activity {
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Uri uri = Uri.parse( url );
+                String newAuthToken = uri.getQueryParameters("auth_token").get(0);
+
+                if (newAuthToken != null) {
+                    finish();
+                    Coiny.coinyLoginViewResponseListener.coinyLoginDidGetToken(newAuthToken);
+                }
+
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @RequiresApi(Build.VERSION_CODES.N)
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 String newAuthToken = request.getUrl().getQueryParameters("auth_token").get(0);
